72c9045d28be340c3424c32d934fdc68ff8aecfd
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-10-30 19:06:04 -0700

    rendered simulation with chart.js

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,49 +1,83 @@
 import React from 'react';
 import simulate from '../../sim.js';
+import { Line, Bar, defaults } from 'react-chartjs-2';
+// defaults.global.animation = false; // disables animating charts by default
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       results: null,
+      chart: null,
+      labels: null,
+      data: null,
+      mostFrequentNum: null,
     };
     this.runSim = this.runSim.bind(this);
   }
 
-  /*____________________________TODO____________________________
-
-    find C3 charts that optimally reflect data.
-
-    refactor sim script to accept chart data format (arrays?)
-
-    display chart in react components
+  /*____________________________TODO____________________________    
+    add user authentication
 
     refactor sim script again to minimize latency
     _____________________________________________________________*/
 
+  componentDidMount() {
+
+  }
   runSim() {
-    this.setState({ results: simulate(100, 50) }, () => {
-      console.log(this.state.results);
+    let results = simulate(100, 50);
+    let labels = [];
+    let data = [];
+    for (const key in results) {
+      labels.push(key);
+      data.push(results[key]);
+    }
+    // pop off numbersGenerated and runtime
+    labels.pop();
+    labels.pop();
+    data.pop();
+    data.pop();
+
+    let mostFrequentNum = [data.indexOf(Math.max(...data)) + 1, Math.max(...data)];
+
+    this.setState({
+      results,
+      labels,
+      data,
+      mostFrequentNum,
     });
   }
 
   render() {
-    if (this.state.results) {
+    if (!this.state.results) {
       return (
         <div>
-          <button onClick={() => this.runSim()}>
-            run again
-          </button>
-          <div>Total Numbers Generated: {this.state.results.numbersGenerated}</div>
-          <div>Simulation Runtime: {this.state.results.runtime.toFixed(3)}ms</div>
+          <button onClick={() => this.runSim()}>Run Simulation</button>
         </div>
       )
     } else {
       return (
-        <div>
+        <div className="app-wrap">
           <button onClick={() => this.runSim()}>
-            run simulation
+            Run Simulation Again
           </button>
+          <div id="charts">
+            <Bar
+              data={{
+                labels: this.state.labels,
+                datasets: [{
+                  label: 'Simulation Results',
+                  data: this.state.data,
+                  borderWidth: 1
+                }]
+              }}
+              // height={150}
+              options={{ maintainAspectRatio: false }}
+            />
+            <h4>{this.state.results.numbersGenerated} numbers were generated in {this.state.results.runtime.toFixed(1)}ms.</h4>
+            <h4>The most frequently occuring number was {this.state.mostFrequentNum[0]} at {this.state.mostFrequentNum[1]} occurrences.</h4>
+          </div>
         </div>
       )
     }
